fddab2bf0c734460ed49fa6e3c71464c036338fa
ELOISA\Eloisa <eloisamedrado_11@hotmail.com>
2020-09-13 17:38:56 -0300

    Adding Cors Mapping configuration.

--- src/main/java/com/kodde/insight/InSightApplication.java
+++ src/main/java/com/kodde/insight/InSightApplication.java
@@ -1,15 +1,42 @@
 package com.kodde.insight;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableCaching
 @EnableFeignClients
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class InSightApplication {
+	
+	@Value("${kodde.insight.allowedOrigin}")
+	private String allowedOrigin;
+	
+	@Value("${kodde.insight.allowedHeaders}")
+	private String allowedHeaders;
+	
+	@Value("${kodde.insight.maxAge}")
+	private Long maxAge;
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/insight/weather")
+                		.allowedOrigins(allowedOrigin)
+                		.allowedHeaders(allowedHeaders)
+                		.maxAge(maxAge);
+            }
+        };
+    }
+    
     public static void main( String[] args ) {
     	SpringApplication.run(InSightApplication.class, args);
     }
